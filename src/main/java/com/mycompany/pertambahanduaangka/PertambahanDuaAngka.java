21cae27cb527fac22aa188d2aa5ef36a1be0b213
Azzailman <azzailmank@gmail.com>
2023-11-04 15:02:25 +0800

    Initial commit

---
+++ src/main/java/com/mycompany/pertambahanduaangka/PertambahanDuaAngka.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertambahanduaangka;
+
+/**
+ *
+ * @author azzai_6qal
+ */
+public class PertambahanDuaAngka {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
